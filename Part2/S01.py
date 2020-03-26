7ba73c27394ef41345bd02136d6748a71adc78b7
Χαράλαμπος Στυλιανού <43931637+Xaralambos12@users.noreply.github.com>
2020-03-26 17:56:02 +0200

    Update S01.py

--- Part2/S01.py
+++ Part2/S01.py
@@ -1,1 +1,11 @@
-
+>>> import re
+>>> rexp = re.compile('^cat')
+>>> m = rexp.search('I see a cat')
+>>> print(m)
+None
+>>> m = rexp.search('category')
+>>> print(m)
+<_sre.SRE_Match object; span=(0, 3), match='cat'>
+>>> print(m.group(0))
+cat
+>>>
